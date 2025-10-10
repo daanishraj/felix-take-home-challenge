39661ed8c9cbea5e3d69f169e0ef2705e464acf2
User <daanishraj@gmail.com>
2025-10-10 11:09:16 +0200

    POC - automatically compute if a tenant is late in paying their rent

--- src/tenant/tenant.resolver.ts
+++ src/tenant/tenant.resolver.ts
@@ -1,9 +1,12 @@
-import {Query, Arg, Int, Resolver, Mutation, Args} from 'type-graphql';
+import {Query, Arg, Int, Resolver, Mutation, Args, FieldResolver, Root} from 'type-graphql';
 
 import Lease from '../lease/lease.entity';
 import {CreateTenantInput, UpdateTenantInput} from './tenant.dto';
 import Tenant from './tenant.entity';
 import TENANT_ERRORS from './tenant.errors';
+import Payment from '../payment/payment.entity';
+import {startOfMonth, endOfMonth, startOfDay, isAfter} from 'date-fns';
+import {Between, LessThanOrEqual, MoreThanOrEqual, IsNull} from 'typeorm';
 
 @Resolver(() => Tenant)
 export default class TenantResolver {
@@ -76,5 +79,53 @@ export default class TenantResolver {
     }
 
     return true;
+  }
+
+
+  @FieldResolver(() => Boolean, {name: 'isRentLate'})
+  async isRentLate(@Root() tenant: Tenant): Promise<boolean> {
+    const now = new Date();
+    return this.computeIsLate(tenant.id, now);
+  }
+
+  @Query(() => Boolean)
+  async isTenantLate(@Arg('id', () => Int) id: number): Promise<boolean> {
+    const tenant = await Tenant.findOne({where: {id}});
+    if (!tenant) {
+      throw new Error(TENANT_ERRORS.INVALID_ID);
+    }
+    const now = new Date();
+    return this.computeIsLate(id, now);
+  }
+
+  private async computeIsLate(tenantId: number, at: Date): Promise<boolean> {
+    const activeLease = await Lease.findOne({
+      where: [
+        {tenantId, start: LessThanOrEqual(at), end: MoreThanOrEqual(at)},
+        {tenantId, start: LessThanOrEqual(at), end: IsNull()},
+      ],
+    });
+
+    if (!activeLease) {
+      return false;
+    }
+
+    const monthStart = startOfMonth(at);
+    const monthEnd = endOfMonth(at);
+
+    const dueDate = startOfDay(monthStart);
+
+    // If we're not past the due date yet for that month, it's not late
+    if (!isAfter(at, dueDate)) {
+      return false;
+    }
+
+    const payments = await Payment.find({
+      where: {tenantId, paymentDate: Between(monthStart, monthEnd)},
+    });
+
+    const monthlyPaid = payments.reduce((sum, p) => sum + (p.amount || 0), 0);
+
+    return monthlyPaid < (activeLease.rent || 0);
   }
 }
